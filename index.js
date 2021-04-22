11d9f0480d24c12bab7e1e7b2aa52b91243e0dfc
faciensamet <82987097+faciensamet@users.noreply.github.com>
2021-04-22 17:36:43 +0900

    add express

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const fs = require("fs");
 const mergeImages = require('merge-images');
 const { Canvas, Image } = require('canvas');
-const { debug } = require("console");
+
+var express = require('express')
+var multer  = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'images')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+var upload = multer({ storage:storage })
+
+var app = express();
+app.use(express.static('merge'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/merge/merge.jpg");
+})
+   
+app.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
 
 var fileList = fs.readdirSync("images");
 
@@ -12,8 +43,8 @@ for(i=0;i<fileList.length;i++)
     imageList.push({ src:"images/"+fileList[i], x: i* 100, y: 0});
 }
 
-mergeImages(imageList, { Canvas: Canvas, Image: Image, format:"image/jpeg", quality:0.6 })
-  .then(b64 => fs.writeFile("merge/merge.jpg", b64.replace(/^data:image\/jpeg;base64,/, ""), 'base64', function(err) { if(err) console.log(err); }));
+mergeImages(imageList, { Canvas: Canvas, Image: Image, format:"image/jpeg", quality:0.3, width:1366, height:768 })
+  .then(b64 => fs.writeFile("merge/merge.jpg", b64.replace(/^data:image\/jpeg;base64,/, ""), 'base64', function(err) { console.log("ok!"); if(err) console.log(err); }));
 
 
 if(!fs.existsSync("images"))
